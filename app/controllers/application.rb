747d4092f4476e3090a77079a6ea05aae4da90ac
Yusuke Kuoka <ykuoka@gmail.com>
2008-09-01 23:53:08 -0700

    [nested_layout][modified] Modified ApplicationController::Base#guard_from_nested_layout  to return true if the controller method called via render_component. This is useful when you want not to use the layout in render_component.

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -22,4 +22,10 @@ class ApplicationController < ActionController::Base
   # Uncomment this to filter the contents of submitted sensitive data parameters
   # from your application log (in this case, all fields with names like "password"). 
   # filter_parameter_logging :password
+
+  alias old_guard_from_nested_layouts guard_from_nested_layouts
+  
+  def guard_from_nested_layouts
+    old_guard_from_nested_layouts or parent_controller
+  end
 end
